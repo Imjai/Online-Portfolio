6ac291b4503e7893aa4135a0eefc05cfaf989cc0
Jai Jain <jainjai1080@gmail.com>
2017-09-19 08:34:59 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,10 +1,10 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
+<?php $Name = $_POST['Name'];
+$Email = $_POST['Email'];
+$Message = $_POST['Message'];
+$formcontent="From: $Name \n Message: $Message";
 $recipient = "jainjai1080@gmail.com";
 $subject = "Contact Form";
-$mailheader = "From: $email \r\n";
+$mailheader = "From: $Email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
 ?>
